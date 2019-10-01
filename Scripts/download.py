195ddf7f4eef549928b0a149d3edf026625deb5a
rh2740 <rh2740@nyu.edu>
2019-10-01 13:55:58 -0400

    10.1.1

--- Scripts/download.py
+++ Scripts/download.py
@@ -5,13 +5,12 @@ Created on 10/01/2019
 
 @author: RH
 """
-import urllib
+import urllib.request
 import pandas as pd
 
 dictkey = pd.read_csv('../tcia_pathology_slides.tsv', sep='\t', header=0)
 
 for idx, row in dictkey.iterrows():
-    filedld = urllib.URLopener()
-    filedld.retrieve(row['slide_url'], '../images/{}.svs'.format(row['slide_id']))
+    urllib.request.urlretrieve(row['slide_url'], '../images/{}.svs'.format(row['slide_id']))
 
 print("done!")
