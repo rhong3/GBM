fdbf9e6c138ba4c613125388a85c48be0287897e
rh2740 <rh2740@nyu.edu>
2019-11-04 17:56:09 -0500

    11.04.3

--- Scripts/cnn_fusion.py
+++ Scripts/cnn_fusion.py
@@ -233,7 +233,7 @@ class INCEPTION:
 
                         if cost <= mintrain and i > 29999:
                             temp_valid = []
-                            for iii in range(10):
+                            for iii in range(100):
                                 x, y, dm = sessa.run(vanext_element)
                                 feed_dict = {self.x_in: x, self.demographic: dm, self.is_train: False}
                                 fetches = [self.pred_cost, self.merged_summary]
